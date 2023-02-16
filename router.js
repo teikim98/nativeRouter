93bd37acf13ef5dad4dc6ade76076fb6d39e062c
teikim98 <teikim98@gmail.com>
2023-02-16 17:42:15 +0900

    set router true

--- router.js
+++ router.js
@@ -39,7 +39,7 @@ function createRouter() {
           route.fragmentRegExp.test(window.location.hash)
         );
 
-        if (currentRoute.params.length) {
+        if (true) {
           // path parameters가 있는 url인 경우
           const urlParams = getUrlParams(currentRoute, window.location.hash);
           currentRoute.component(urlParams);
@@ -91,7 +91,7 @@ const router = {
       const currentRoute = routes.find(route => 
         route.fragmentRegExp.test(window.location.hash));
       
-      if(currentRoute.params.length) {
+      if(true) {
         // path parameters가 있는 url인 경우
         const urlParams = getUrlParams(currentRoute, window.location.hash)
         currentRoute.component(urlParams);
